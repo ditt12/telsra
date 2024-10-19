e73d3374106b9dae2912c86f1c703ce5f26e5137
asyraff <145573395+ditt12@users.noreply.github.com>
2024-10-19 19:34:17 +0700

    Update telsraf.js

--- telsraf.js
+++ telsraf.js
@@ -1,13 +1,17 @@
-// telsraf.js
+// telsra.js
 const Telsra = require('./src/index');
 
-const bot = new Telsra('TOKEN_BOT');
+const bot = new Telsra('YOUR_TELEGRAM_BOT_TOKEN');
 
 bot.startPolling((update) => {
-    const chatId = update.message.chat.id;
-    const text = update.message.text;
+    if (update.message && update.message.chat) {
+        const chatId = update.message.chat.id;
+        const text = update.message.text;
 
-    if (text === '/start') {
-        bot.sendMessage(chatId, 'Welcome to My Telegram Bot!');
+        if (text === '/start') {
+            bot.sendMessage(chatId, 'Welcome to My Telegram Bot!');
+        }
+    } else {
+        console.log("Received update without message:", update);
     }
 });
